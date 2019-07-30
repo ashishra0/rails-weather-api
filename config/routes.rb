4d0356e4918af3e3f8a8136e28950a0437cce467
Ashish Rao <ashishrao2598@gmail.com>
2019-07-30 18:06:45 +0530

    add knock gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  namespace :v1, defaults: { format: :json } do
+  namespace :v1 do
+    post 'user_token' => 'user_token#create'
     resources :locations do
       resources :recordings
     end
   end
+  resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
